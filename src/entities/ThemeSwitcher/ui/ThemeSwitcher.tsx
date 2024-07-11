f408f38a84485394712bc9d703ec7695a82f7a4c
Roman Druzhinin <karamount11@gmail.com>
2024-07-12 01:51:33 +0300

    feature: компонент ThemeSwitcher
    *Шаблон компонента заменен на полноценный(без цветов)

--- src/entities/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/entities/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,13 +1,34 @@
-import React, { FC, useContext } from 'react';
+import { FC } from 'react';
 import classes from './ThemeSwitcher.module.scss';
 import { changeColorsTheme } from '../lib/helpers/changeColorsTheme';
 import { useColorsTheme } from '../lib/hooks/useColorsTheme';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 
 const ThemeSwitcher: FC = () => {
   const { colorsTheme, setColorsTheme } = useColorsTheme();
 
   return (
-    <div onClick={() => changeColorsTheme(colorsTheme, setColorsTheme)}>blblbl</div>
+    <button
+      onClick={(e) => {
+        e.preventDefault(); 
+        changeColorsTheme(colorsTheme, setColorsTheme);
+      }}
+      className={classes.themeSwitcher}
+    >
+      <span className={
+        colorsTheme === 'dark' 
+        ? 
+        classes.themeSwitcher__container_dark 
+        : 
+        classes.themeSwitcher__container_light
+      }>
+        <FontAwesomeIcon 
+          icon={colorsTheme === 'dark' ? faMoon : faSun} 
+          className={classes.themeSwitcher__icon}
+        />
+      </span>
+    </button>
   );
 };
 
