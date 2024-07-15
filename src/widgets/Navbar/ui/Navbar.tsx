8c00b2af88b7d30e95883b03ef5f80787b7afff2
Roman Druzhinin <karamount11@gmail.com>
2024-07-16 00:56:41 +0300

    feature: компонент Navbar
    *Заглушка заменена полноценным компонентом

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,16 +1,38 @@
 import { FC } from 'react'
 import { Link } from 'react-router-dom';
 import classes from './Navbar.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar: FC = () => {
   return (
-    <div className={classes.navbar}>
-      <Link className={classes.navbar__link} to={'/main'}>Главная</Link>
-      <Link className={classes.navbar__link} to={'/newtours'}>Открытия сезона</Link>
-      <Link className={classes.navbar__link} to={'/hottours'}>Горячие туры</Link>
-      <Link className={classes.navbar__link} to={'/prices'}>Калькулятор цен</Link>
-      <Link className={classes.navbar__link} to={'/about'}>О нас</Link>
-    </div>
+    window.innerWidth < 600
+
+    ?
+
+    <nav className={classes.navbar}>
+      <FontAwesomeIcon icon={faXmark} className={classes.navbar__icon}/>
+
+      <p className={classes.navbar__paragraph}>
+        Space Travels Agency
+      </p>
+
+      <Link className={classes.navbar__button} to={'/main'}>Главная</Link>
+      <Link className={classes.navbar__button} to={'/newtours'}>Открытия сезона</Link>
+      <Link className={classes.navbar__button} to={'/hottours'}>Горячие туры</Link>
+      <Link className={classes.navbar__button} to={'/prices'}>Калькулятор цен</Link>
+      <Link className={classes.navbar__button} to={'/about'}>О нас</Link>
+    </nav>
+
+    :
+
+    <nav className={classes.navbar}>
+      <Link className={classes.navbar__button} to={'/main'}>Главная</Link>
+      <Link className={classes.navbar__button} to={'/newtours'}>Открытия сезона</Link>
+      <Link className={classes.navbar__button} to={'/hottours'}>Горячие туры</Link>
+      <Link className={classes.navbar__button} to={'/prices'}>Калькулятор цен</Link>
+      <Link className={classes.navbar__button} to={'/about'}>О нас</Link>
+    </nav>
   );
 };
 
